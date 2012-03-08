0009720a6616478ba02382acd2855e2d87789872
Gabe Smith <gabe@paperlesspost.com>
2012-03-08 13:28:10 -0500

    problem 5 in ruby

---
+++ 5.rb
@@ -0,0 +1,21 @@
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+# 
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+RANGE = (11..20)
+
+def divis?(num)
+  RANGE.each{|i| return false if num%i != 0 }
+  return true
+end
+
+val = [*RANGE].inject(1){|sum, i| sum *= i}
+RANGE.each do |divisor|
+  val /= divisor while divis?(val/divisor)
+end
+puts val
+
+# => 232792560
+# real    0m0.008s
+# user    0m0.005s
+# sys     0m0.002s
