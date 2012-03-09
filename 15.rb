25e38b9c8c2d23de29ba2cb8944f95133a1bdaac
Gabe Smith <gabe@paperlesspost.com>
2012-03-09 16:57:39 -0500

    problem 15 in ruby

---
+++ 15.rb
@@ -0,0 +1,20 @@
+# Starting in the top left corner of a 2x2 grid, there are 6
+# routes (without backtracking) to the bottom right corner.
+# 
+# How many routes are there through a 20x20 grid?
+
+def factorial(n)
+  [*1..n].inject(:*)
+end
+
+# (2n choose n) => 2n! / (n! * n!)
+def count_paths(grid_size)
+  factorial(2*grid_size) / (factorial(grid_size) * factorial(grid_size))
+end
+
+puts count_paths(20)
+
+# => 137846528820
+# real    0m0.009s
+# user    0m0.004s
+# sys     0m0.002s
