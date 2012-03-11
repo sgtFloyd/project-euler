f4a6ca4b6ce7667118c98ce644fbf31557de509b
Gabe Smith <g@sgtfloyd.com>
2012-03-11 00:39:46 -0500

    problem 20 in ruby

---
+++ 20.rb
@@ -0,0 +1,13 @@
+# n! means n x (n - 1) x ... x 3 x 2 x 1
+# 
+# For example, 10! = 10 x 9 x ... x 3 x 2 x 1 = 3628800,
+# and the sum of the digits in the number 10! is
+#   3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+#
+# Find the sum of the digits in the number 100!
+
+def factorial(n)
+  [*1..n].inject(:*)
+end
+
+puts factorial(100).to_s.split('').inject(0){|sum, n| sum + n.to_i}
