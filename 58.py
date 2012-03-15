eab6bac4f7eebbe727eb8d66412f4471463a0f33
Gabe Smith <gabe@paperlesspost.com>
2012-03-15 17:08:08 -0400

    efficient prime determination. need efficient diagonal calculation

--- 58.py
+++ 58.py
@@ -19,16 +19,15 @@
 # diagonals first falls below 10%?
 
 import math
-
-# memo-ized sieve of eratosthenes
 primes = [False, False, True] # initial values
-def sieve(n):
+def sieve(n): # memoized, optimized sieve of eratosthenes
   global primes; lower = len(primes)
-  if n+1 > lower: # extend storage, even indexes non-prime
+  if n+1 > lower:
+    # extend storage, even #s are automatically non-prime
     primes += [True, False] * ((n-lower)/2+1)
   for i in xrange(3, int(math.sqrt(n)+1), 2):
     if primes[i]:
-      for j in [x for x in xrange(2*i, n+1, i) if x>=lower]:
+      for j in [x for x in xrange(3*i, n+1, 2*i) if x>=lower]:
         primes[j] = False
   # return [i for i, is_prime in enumerate(primes) if is_prime]
 
@@ -40,16 +39,53 @@ def diagonals(size):
               i**2 + (i+1),   # bottom-left diagonal
               i**2 - (i-1) ]  # upper-right diagonal
             for i in xrange(2, size+1, 2)
-          ] + [[1]] )         # center 1
+          ] #+ [[1]]          # center 1 - never prime!
+        )
 
-# TODO: uses Miller-Rabin primality test for number greater than 999984
 def is_prime(n):
   if n < 999985: return primes[n] # use cache
-  if (n % 2 == 0) or (n % 3 == 0): return False
+  if n%2 == 0 or n%3 == 0 or n%5 == 0 or n%7 == 0: return False
+  return miller_rabin(n)
+
+
+import random
+
+def miller_rabin(n):
+  # compute s and d
+  d = n - 1; s = 0
+  while d % 2 == 0:
+    d >>= 1; s += 1
+  for a in a_vals(n):
+    if not miller_rabin_pass(a, s, d, n):
+      return False
+  return True
+
+def miller_rabin_pass(a, s, d, n):
+  a_to_power = pow(a, d, n)
+  if a_to_power == 1: return True
+  for _ in xrange(s-1):
+    if a_to_power == n - 1: return True
+    a_to_power = (a_to_power * a_to_power) % n
+  return a_to_power == n - 1
+
+# deterministic for values < 341,550,071,728,321
+def a_vals(n):
+  if n < 1373653:         return [2, 3]                   # n < 1,373,653
+  if n < 9080191:         return [31, 73]                 # n < 9,080,191
+  if n < 4759123141:      return [2, 7, 61]               # n < 4,759,123,141
+  if n < 2152302898747:   return [2, 3, 5, 7, 11]         # n < 2,152,302,898,747
+  if n < 3474749660383:   return [2, 3, 5, 7, 11, 13]     # n < 3,474,749,660,383
+  if n < 341550071728321: return [2, 3, 5, 7, 11, 13, 17] # n < 341,550,071,728,321
+  return [random.randint(1,n-1) for _ in xrange(0,20)]
 
 def prime_percent(spiral_size):
   return sum([is_prime(d)
                 for d in diagonals(spiral_size)
-            ]) / float(2*spiral_size-1)
+            ]) / (2*spiral_size-1.0) # ensure float
 
 sieve(999983) # cache prime numbers 0 - 999,983
+i=26501
+while True:
+  i += 2
+  pp = prime_percent(i)
+  print "%s: %s" % (str(i), str(pp))
