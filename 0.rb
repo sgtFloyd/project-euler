23c304f40f3eaabb4cf792ae746103385092dfa4
Gabe Smith <gabe@paperlesspost.com>
2012-03-20 16:50:58 -0400

    useful ruby algorithms - modular exponentiation

--- 0.rb
+++ 0.rb
@@ -10,3 +10,15 @@ def sieve(n)
   end
   $primes.map.with_index{|t, i| i if t}.compact
 end
+
+# modular exponentiation
+def mod_pow(base, exp, mod)
+  res = 1
+  while exp > 0
+    odd_exp = exp % 2 == 1
+    res = (res * base) % mod if odd_exp
+    base = (base * base) % mod
+    exp = exp / 2
+  end
+  return res
+end
