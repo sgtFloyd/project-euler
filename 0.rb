cfe0abac217929340b2e2e4002583e7b9b9dbd56
Gabe Smith <gabe@paperlesspost.com>
2012-03-21 12:02:07 -0400

    solve quadratics with ruby

--- 0.rb
+++ 0.rb
@@ -22,3 +22,28 @@ def mod_pow(base, exp, mod)
   end
   return res
 end
+
+# solve for n: 0 = an^2 + bn + c
+def solve_quad(a, b, c)
+  sqrt = Math.sqrt(b**2 - 4*a*c)
+  return [(-b + sqrt)/(2*a),
+          (-b - sqrt)/(2*a)]
+end
+
+# x = n(n+1)/2 => 0 = n^2 + n - 2x
+def is_triangle(x)
+  solution = solve_quad(1, 1, -2*x)
+  solution.max % 1 == 0
+end
+
+# x = n(3n-1)/2 => 0 = 3n^2 - n - 2x
+def is_pentagonal(x)
+  solution = solve_quad(3, -1, -2*x)
+  solution.max % 1 == 0
+end
+
+# x = n(2n-1) => 0 = 2n^2 - n - x
+def is_hexagonal(x)
+  solution = solve_quad(2, -1, -x)
+  solution.max % 1 == 0
+end
